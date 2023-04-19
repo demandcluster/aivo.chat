d38c31451a9149037c65a7e8cbae35ab805dd7fd
Ron W. van Etten <rondlite@gmail.com>
2023-04-19 14:09:31 +0200

    discord bot images

--- srv/discord/character.ts
+++ srv/discord/character.ts
@@ -27,44 +27,6 @@ module.exports = {
 
             const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
-await lib.discord.channels['@0.3.2'].messages.create({
-  "channel_id": `${context.params.event.channel_id}`,
-  "content": `This is the bot you wanted`,
-  "tts": false,
-  "allowed_mentions": {
-    "replied_user": true,
-    "parse": [
-      "users"
-    ]
-  },
-  "embeds": [
-    {
-      "type": "rich",
-      "title": `Botname`,
-      "description": `The description`,
-      "color": 0x00FFFF,
-      "fields": [
-        {
-          "name": `Body`,
-          "value": `12`,
-          "inline": true
-        },
-        {
-          "name": `Age`,
-          "value": `24`,
-          "inline": true
-        }
-      ],
-      "image": {
-        "url": `https://yt3.googleusercontent.com/r1CCbxgxs0mEVSm7fnjPHPmyOcC_9s9P9dhKzI3hMsH6FOrNQ0UbkiTEKcUOqibrN7G-9qgg_Ig=s176-c-k-c0x00ffffff-no-rj`,
-        "height": 0,
-        "width": 0
-      },
-      
-      "url": `https://profileurl.url`
-    }
-  ]
-});
             // Async rendering... 
           const embed = {
             "title": character.name,
